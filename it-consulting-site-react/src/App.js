108f206cd21fc6d6340f4d36553483c05d0b9f37
kristamcmanus <k.mcmanus310@gmail.com>
2021-09-11 16:31:23 -0400

    Add folders for components, pages, images, and videos

--- it-consulting-site-react/src/App.js
+++ it-consulting-site-react/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
