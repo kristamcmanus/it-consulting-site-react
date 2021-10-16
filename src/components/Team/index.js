96d09e0ca3f496035bc03239c5aeb40e382ec292
kristamcmanus <k.mcmanus310@gmail.com>
2021-10-16 16:46:33 -0400

    Add professional headshots to team section

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Icon1 from '../../images/svg-1.svg';
-import Icon2 from '../../images/svg-2.svg';
-import Icon5 from '../../images/svg-5.svg';
+import Team1 from '../../images/team-1.jpg';
+import Team2 from '../../images/team-2.jpg';
+import Team3 from '../../images/team-3.jpg';
 
 import { TeamContainer, TeamH1, TeamWrapper, TeamCard, TeamIcon, TeamH2, TeamP } from './TeamElements';
 
@@ -11,17 +11,17 @@ const Team = () => {
       <TeamH1>Our Team</TeamH1>
       <TeamWrapper>
         <TeamCard>
-          <TeamIcon src={Icon1} alt='IT consulting'/>
+          <TeamIcon src={Team1} alt='IT consulting'/>
           <TeamH2>IT Consulting</TeamH2>
           <TeamP>You might consider hiring someone internally, but often times outsourcing your IT services a smarter route to go. We can help.</TeamP>
         </TeamCard>
         <TeamCard>
-          <TeamIcon src={Icon2} alt='network security'/>
+          <TeamIcon src={Team2} alt='network security'/>
           <TeamH2>Network Security</TeamH2>
           <TeamP>Businesses are targets for cybercriminals to launch highly sophisticated cyber threats against, particularly the small to mid-sized firm.</TeamP>
         </TeamCard>
         <TeamCard>
-          <TeamIcon src={Icon5} alt='cloud solutions'/>
+          <TeamIcon src={Team3} alt='cloud solutions'/>
           <TeamH2>Cloud Solutions</TeamH2>
           <TeamP>Glo Tech can create a cloud-based technology infrastructure that adapts to and evolves alongside your business.</TeamP>
         </TeamCard>
