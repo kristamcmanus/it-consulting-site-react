2f62a6e171bcfcce06b0a21e5cca95ec7f7e2f45
kristamcmanus <k.mcmanus310@gmail.com>
2021-10-17 16:10:21 -0400

    Add Our Team nav link to sidebar menu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -12,6 +12,7 @@ const Sidebar = ({ isOpen, toggle }) => {
           <SidebarLink to='about' onClick={toggle}>About</SidebarLink>
           <SidebarLink to='our process' onClick={toggle}>Our Process</SidebarLink>
           <SidebarLink to='services' onClick={toggle}>Services</SidebarLink>
+          <SidebarLink to='team' onClick={toggle}>Our Team</SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
           <SidebarRoute to='gloria.karlinchak@gmail.com'>Contact Us</SidebarRoute>
