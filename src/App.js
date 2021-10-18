95467832cb81cc7232c84bcaaac61d22f0b637f5
kristamcmanus <k.mcmanus310@gmail.com>
2021-10-18 11:44:01 -0400

    Change route path to project path name in attempt to fix deployment bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Home} exact />
+        <Route path='/it-consulting-site-react-client' component={Home} exact />
       </Switch>
     </Router>
   );
